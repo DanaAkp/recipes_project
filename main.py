6c7fb9bec35841d83d83fbc5eda85d05a4bbb124
Danagul.Akpayeva <Danagul.Akpayeva@waveaccess.ru>
2022-05-31 14:36:18 +0500

    added test routes and response models

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Query, Path, Body
+from schemas import Ingredient, Recipe
 
 app = FastAPI()
 
@@ -11,3 +12,28 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post('/ingredients', response_model=Ingredient)
+def create_ingredient(item: Ingredient):
+    return item
+
+
+@app.post('/recipes')
+def create_recipes(item: Recipe):
+    return item
+
+
+@app.get('/ingredients')
+def get_ingredient(q: str = Query(..., max_length=5, min_length=1, description='test query.')):
+    return q
+
+
+@app.get('/ingredient/{id}')
+def get_ingredient_by_id(id: int = Path(..., gt=1, le=20)):
+    return {'id': id}
+
+
+@app.post('/test_body')
+def post_body(test_body: int = Body(..., embed=True)):
+    return {'body': test_body}
