cd48c4be26a3f230a1ad22bc9aa9c8e3fcb3adf2
Danagul.Akpayeva <Danagul.Akpayeva@waveaccess.ru>
2022-07-04 12:44:46 +0500

    fix getting recipes

--- recipe_parser/calorizator.py
+++ recipe_parser/calorizator.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from recipes_adapter import AbstractRecipeParser
+from recipe_parser.recipes_adapter import AbstractRecipeParser
 
 
 class Calorizator(AbstractRecipeParser):
@@ -33,12 +33,6 @@ class Calorizator(AbstractRecipeParser):
         buf = tree.xpath("//div[@class='field field-type-text recipes-ingredients']/div/div/ul/li")
         ingredients = [i.text for i in buf]
 
-        buf = tree.xpath("//div[@class='field field-type-text field-field-recipe-kpfc']//p")
-        calories_recipe = buf[1].text_content()  # 'Калории: 101.1 ккал.'
-        protein_recipe = buf[2].text_content()  # 'Белки: 4.1 гр.'
-        fat_recipe = buf[3].text_content()  # 'Жиры: 4.3 гр.'
-        carbohydrates_recipe = buf[4].text_content()  # 'Углеводы: 12 гр.'
-
         buf = tree.xpath("//div[@class='recipes-portions']//span")
         portions = buf[0].text_content()
 
@@ -48,22 +42,27 @@ class Calorizator(AbstractRecipeParser):
             tr = list(filter(lambda x: x != '',
                              i.text_content().split('\n')))
             mera = tr[0]
-            weight = tr[0]
-            protein_product = tr[0]
-            fat_product = tr[0]
-            carbohydrates_product = tr[0]
-            calories_product = tr[0]
+            weight = tr[1]
+            protein_product = tr[2]
+            fat_product = tr[3]
+            carbohydrates_product = tr[4]
+            calories_product = tr[5]
             products.append(dict(mera=mera, weight=weight, protein_product=protein_product, fat_product=fat_product,
                                  carbohydrates_product=carbohydrates_product, calories_product=calories_product))
 
         buf = tree.xpath("//div[@class='field field-type-text field-field-recipe-table']//tfoot//tr")
-        total = list(filter(lambda x: x != '', buf[0].text_content().split('\n')))
-        one_portion = list(filter(lambda x: x != '', buf[1].text_content().split('\n')))
-        one_hundred_grams = list(filter(lambda x: x != '', buf[2].text_content().split('\n')))
+        filter_lambda = lambda x: x not in ('', '\xa0')
+        total = list(filter(filter_lambda, buf[0].text_content().split('\n')))
+        one_portion = list(filter(filter_lambda, buf[1].text_content().split('\n')))
+        one_hundred_grams = list(filter(filter_lambda, buf[2].text_content().split('\n')))
 
         return dict(
-            total=total, one_hundred_grams=one_hundred_grams, one_portion=one_portion,
-            name_recipe=name_recipe, recipe_steps=recipe_steps, products=products, portions=portions,
-            calories_recipe=calories_recipe, protein_recipe=protein_recipe, fat_recipe=fat_recipe,
-            carbohydrates_recipe=carbohydrates_recipe, ingredients=ingredients
+            total=total,
+            one_hundred_grams=one_hundred_grams,
+            one_portion=one_portion,
+            name_recipe=name_recipe,
+            recipe_steps=recipe_steps,
+            products=products,
+            portions=portions,
+            ingredients=ingredients
         )
