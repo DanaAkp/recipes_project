b6dd2ca429af897a96b86f7673146ce7d3b0cab9
Danagul.Akpayeva <Danagul.Akpayeva@waveaccess.ru>
2022-07-05 12:14:02 +0500

    Revert "error circular import or not table"
    This reverts commit 5c8fce715aeb3a351daf83b3acd60bf2a96a0951.

--- database/models.py
+++ database/models.py
@@ -16,51 +16,28 @@ class RecipeType(enum.Enum):
     drinks = 'Напитки'
 
 
-class Product(db.Base):
-    __tablename__ = 'product'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True, nullable=False)
+product = Table('product', db.metadata,
+                Column('id', Integer, primary_key=True),
+                Column('name', String, nullable=False, unique=True))
 
+recipe = Table('recipe', db.metadata,
+               Column('id', Integer, primary_key=True),
+               Column('name', String, nullable=False, unique=True))
 
-db.metadata.create_all(bind=db.engine)
-db.session.add(Product(name='Potato'))
-db.session.commit()
-db.session.query(Product).all()
-print()
+element = Table('element', db.metadata,
+                Column('id', Integer, primary_key=True),
+                Column('name', String, nullable=False, unique=True))
 
+recipe_product = Table('recipe_product', db.metadata,
+                       Column('recipe_id', Integer, ForeignKey('recipe.id'), primary_key=True),
+                       Column('product_id', Integer, ForeignKey('product.id'), primary_key=True),
+                       Column('calories', Float),
+                       Column('fats', Float),
+                       Column('carbohydrates', Float),
+                       Column('description', String, nullable=False)
+                       )
 
-# product = Table(
-#     'product', db.metadata,
-#     Column('id', Integer, primary_key=True),
-#     Column('name', String, nullable=False, unique=True)
-# )
-#
-# recipe = Table(
-#     'recipe',
-#     db.metadata,
-#     Column('id', Integer, primary_key=True),
-#     Column('name', String, nullable=False, unique=True)
-# )
-#
-# element = Table(
-#     'element', db.metadata,
-#     Column('id', Integer, primary_key=True),
-#     Column('name', String, nullable=False, unique=True)
-# )
-#
-# recipe_product = Table(
-#     'recipe_product', db.metadata,
-#     Column('recipe_id', Integer, ForeignKey('recipe.id'), primary_key=True),
-#     Column('product_id', Integer, ForeignKey('product.id'), primary_key=True),
-#     Column('calories', Float),
-#     Column('fats', Float),
-#     Column('carbohydrates', Float),
-#     Column('description', String, nullable=False)
-# )
-#
-# element_product = Table(
-#     'element_product', db.metadata,
-#     Column('element_id', Integer, ForeignKey('element.id'), primary_key=True),
-#     Column('product_id', Integer, ForeignKey('product.id'), primary_key=True),
-#     Column('percents', Float, nullable=False)
-# )
+element_product = Table('element_product', db.metadata,
+                        Column('element_id', Integer, ForeignKey('element.id'), primary_key=True),
+                        Column('product_id', Integer, ForeignKey('product.id'), primary_key=True),
+                        Column('percents', Float, nullable=False))
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlalchemy
-from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy import create_engine
 from fastapi import FastAPI
+
+from database.connect_to_db import DBConnect
 
 app = FastAPI()
 
@@ -10,18 +9,7 @@ app = FastAPI()
 async def startup_event():
     pass
 
-
-class DBConnect:
-    def __init__(self, db_url: str):
-        self.engine = create_engine(db_url, echo=True, future=True)
-        self.metadata = sqlalchemy.schema.MetaData()
-        self.Base = declarative_base(bind=self.engine)
-        self.connect = self.engine.connect()
-        session = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
-        self.session = session()
-
-
 db = DBConnect("sqlite:///test.db")
 
-from database.models import *
 db.metadata.create_all(bind=db.engine)
+
