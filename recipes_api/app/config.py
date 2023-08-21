2110337dad26b23b93a987a37610b3feada16e8e
Danagul Akpaeva <Danagul.Akpayeva@waveaccess.ru>
2023-08-21 15:41:53 +0300

    added file docker-compose.yml

--- recipes_api/app/config.py
+++ recipes_api/app/config.py
@@ -2,8 +2,8 @@ import os
 
 DATABASE_USER_NAME = os.environ.get('DATABASE_USER_NAME', 'postgres')
 DATABASE_USER_PASSWORD = os.environ.get('DATABASE_USER_PASSWORD', 'postgres')
-DATABASE_NAME = os.environ.get('DATABASE_NAME', 'recipes')
-DATABASE_PORT = int(os.environ.get('DATABASE_PORT', 5433))
+DATABASE_NAME = os.environ.get('DATABASE_NAME_RECIPES', 'recipes')
+DATABASE_PORT = int(os.environ.get('DATABASE_PORT', 5432))
 DATABASE_HOST = os.environ.get('DATABASE_HOST', 'localhost')
 DATABASE_URL = f"postgresql://" \
                f"{DATABASE_USER_NAME}:{DATABASE_USER_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
